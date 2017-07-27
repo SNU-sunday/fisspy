61157e51e097da3b520ee6dbdeac8f1df4afeb81
Juhyeong Kang <jhkang@astro.snu.ac.kr>
2017-07-27 13:16:37 +0900

    debug setup tool

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ URL = metadata.get('url', 'http://fiss.snu.ac.kr')
 LONG_DESCRIPTION = "FISSPy is the python packages to analyze the GST/FISS data file."
 
 # VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-VERSION = '0.8.2'
+VERSION = '0.8.4'
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
