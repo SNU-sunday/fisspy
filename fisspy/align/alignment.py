ebf520449f4bf290fc2f6587d1a7b78b4850da16
Juhyung Kang <jhkang@astro.snu.ac.kr>
2024-04-09 20:36:45 +0900

    backup

--- fisspy/align/alignment.py
+++ fisspy/align/alignment.py
@@ -1,14 +1,17 @@
 from .base import AlignOffset, rotImage, shiftImage
 import numpy as np
 import matplotlib.pyplot as plt
+from ..read import FISS
 
-
-def calAlignPar(lcamA, lcamB, refFrame=None, save=False):
-    if len(lcamB) and nf:
-        nf = len(lcamA)
-        raise ValueError("Inputs fHas and FCas in FissAlign should have the same number of elements!")
+def calAlignPar(lcam, refFrame=None):
+    nf = len(lcam)
+    if refFrame is None:
+        refFrame = nf//2
+    rf = refFrame
+    fissr = FISS(lfiles[rf], wvCalibMethod='simple')
+    imr = fissr.data[..., 50:55].mean(2)
     
-def align2cams(imA, imB):
+def alignCams(imA, imB, refCam='A'):
     None
 
 def alignRot(flist):
@@ -20,7 +23,44 @@ def makeAlignCube(save=False):
 class Align:
     """
     """
-    def __init__(self, lcamA, lcamB, refFrame=None):
-        None
+    def __init__(self, lcamA, lcamB, refCam='A', refFrame=None, align=True):
+        """
+        Parameters
+        ----------
+        lcamA: `list`
+            List of the camA files
+        lcamB: `list`
+            List of the camB files
+        refCam: `str`
+            Reference camera for the alignment.
+            Either 'A' or 'B'.
+            Default is 'A'.
+        refFrame: `int`
+            Reference frame number.
+            Default is the middle frame of the input list.
+        Returns
+        -------
+        """
+        nfA = len(lcamA)
+        nfB = len(lcamB)
+        if nfA <= 1 or nfB <=1:
+            raise ValueError(f"The number of elements of either lcamA or lcamB should be larger than 1.\n    Note) nfA={nfA}, nfB={nfB} .")
+        if nfA != nfB:
+            raise ValueError(f"lcamA and lcamB should have the same number of elements.")
+        self.lcamA = lcamA
+        self.lcamB = lcamB
+        self.nf = nfA
+        self.refCam = refCam.upper()
+        if self.refCam != 'A' or self.refCam != 'B':
+            raise ValueError("refCam should be either 'A' or 'B'.")
+        
+        if refFrame is None:
+            refFrame = self.nf//2
+        if refFrame < 0 or refFrame >= self.nf:
+            raise ValueError("refFrame should be 0<=refFrame<nf, where nf is the number of files.")
+        
+        self.refFrame = refFrame
+
+
         
         
