017187942f5ddbc91c0c90aeee01009cbed88627
Juhyung Kang <jhkang@astro.snu.ac.kr>
2024-04-21 15:29:42 +0900

    v1.0.2

--- fisspy/analysis/forecast.py
+++ fisspy/analysis/forecast.py
@@ -1,13 +1,13 @@
 from statsmodels.tsa.ar_model import AutoReg
 import numpy as np
 from interpolation.splines import LinearSpline, CubicSpline
-# from scipy.interpolate import interp1d
+from scipy.interpolate import interp1d
 
 __author__ = "Juhyung Kang"
 __email__ = "jhkang0301@gmail.com"
 __all__ = ["ARcast"]
 
-def ARcast(data, time, dt=False, axis=-1, missing=0):
+def ARcast(data, time, dt=False):
     """
     Forecast the data by using AutoRegressive method.
     
@@ -19,111 +19,88 @@ def ARcast(data, time, dt=False, axis=-1, missing=0):
     data : `~numpy.ndarray`
         n dimensional data.
         Data must have the same number of elements to the time.
-    time : astropy.time.core.Time
+        Data should be (nt,...) where nt is the size in the time axis.
+    time : `~numpy.ndarray`
         The time for the each data points.
-    dt : (optional) float
+    dt : `float` (optional)
         An Interval of the time between each data in second unit.
-    axis : (optional) int
-        An axis to forecast.
-    missing : (optional) float
-        The missing value of the data.
-        It may be due to data alignment.
 
     Returns
     -------
-    ARdata : ~numpy.ndarray
+    ARdata : `~numpy.ndarray`
         Autoregressived data.
         It must be larger elements then input data.
-    tf : ~numpy.ndarray
+    tf : `~numpy.ndarray`
         Time the forecasted ARdata points.
     
-    Notes
-    -----
-    Input time must be the astropy.time.core.Time, 
-    but output time is the ~numpy.ndarray.
     """
-    None
-
-# def ARcast_old(data,time,dt=False,axis=-1,missing=0):
-#     """
-#     Forecast the data by using AutoRegressive method.
+    if not dt:
+        tmp = np.roll(time, -1) - time
+        dt = np.median(tmp[:-1])
     
-#     The code automatically find the unevenly sampled data point, 
-#     and then forecast the that point by using AR method.
-    
-#     Parameters
-#     ----------
-#     data : ~numpy.ndarray
-#         n dimensional data.
-#         Data must have the same number of elements to the time.
-#     time : astropy.time.core.Time
-#         The time for the each data points.
-#     dt : (optional) float
-#         An Interval of the time between each data in second unit.
-#     axis : (optional) int
-#         An axis to forecast.
-#     missing : (optional) float
-#         The missing value of the data.
-#         It may be due to data alignment.
-    
-#     Returns
-#     -------
-#     ARdata : ~numpy.ndarray
-#         Autoregressived data.
-#         It must be larger elements then input data.
-#     tf : ~numpy.ndarray
-#         Time the forecasted ARdata points.
-    
-#     Notes
-#     -----
-#     Input time must be the astropy.time.core.Time, 
-#     but output time is the ~numpy.ndarray.
-    
-    
-#     Example
-#     -------
-#     >>> from fisspy.analysis.forecast import ARcast
-#     >>> ARdata, tf = ARcast(data,t,dt=20.,axis=1)
-#     """
-#     if not dt:
-#         dt=(time[1]-time[0]).value
-    
-#     shape=list(data.shape)
-#     shape0=list(data.shape)
-#     if shape[axis]!=len(time):
-#         raise ValueError('The size of data is different from the size of time.')
+    shape = data.shape
+    if shape[0]!=len(time):
+        raise ValueError('The size of data is different from the size of time.')
         
-#     t=(time-time[0])*24*3600
-#     t=t.value
-#     tf=np.arange(t[0],t[-1],dt,dtype=float)
+    t = (time-time[0])
+    tf = np.arange(t[0], t[-1], dt, dtype=float)
     
-#     interp=interp1d(t,data,axis=axis)
-#     datai=interp(tf)
+    interp = interp1d(t, data, kind='cubic', axis=0)
+    datai = interp(tf)
+    shapei = datai.shape
+    nt = len(tf)
     
-#     shape.pop(axis)
-#     ind=[shape0.index(i) for i in shape]
-#     ind=[axis]+ind
-#     datat=datai.transpose(ind)
+    if data.ndim == 1:
+        nl = 1
+    else:
+        nl = int(datai.size//nt)
+    datat = datai.reshape((nt, nl))
+
+    # shapei=datat.shape
+    # datat=datat.reshape((shapei[0],np.prod(shapei[1:])))
+    # shapet=datat.shape
     
-#     shapei=datat.shape
-#     datat=datat.reshape((shapei[0],np.prod(shapei[1:])))
-#     shapet=datat.shape
+    td = t - np.roll(t,1)
+    addi = np.where(td >= dt*1.5)[0]
+
+    nad = len(addi)
+    if nad == 0:
+        print("There is no empty element. Just interpolate to evenly sample the data.")
+        return datai
+
+    npredict = (td[addi]/dt + 0.5).astype(int)-1
     
-#     td=t-np.roll(t,1)
-#     addi=np.where(td >= dt*2)[0]
+    for i in range(nad):
+        ts = np.abs(tf - t[addi[i]-1]).argmin()+1
+        te = ts+npredict[i]
+        
+        if ts//2 < 10:
+            fskip = True
+        else:
+            fskip = False
+        if (nt - te)//2 < 10:
+            bskip = True
+        else:
+            bskip = False
+
+        bF0 = (np.arange(npredict[i])+1)/(npredict[i]+1)
+        fF0 = 1 - bF0
+        bF = (1-bskip)*(bF0 + fskip*fF0)
+        fF = (1-fskip)*(fF0 + bskip*bF0)
+        
+        for l in range(nl):
+            y = datat[:,l]
+            if not fskip:
+                fm = AutoReg(y[:ts], lags=10).fit()
+                fp = fm.forecast(npredict[i])
+            else:
+                fp = np.zeros(npredict[i])
+            if not bskip:
+                bm = AutoReg(y[te:][::-1], lags=10).fit()
+                bp = bm.forecast(npredict[i])
+            pred = fF*fp + bF*bp[::-1]
+            datat[ts:te,l] = pred
+
+    datat = datat.reshape((shapei))
     
-#     for wh in addi:
-#         for i in range(shapet[1]):
-#             y=datat[:,i]
-#             wh2=wh+int(td[wh]/dt-1)
-#             if (y==missing).sum()<4:
-#                 bar=AR(y)
-#                 car=bar.fit()
-#                 dar=car.predict(int(wh),int(wh2))
-#                 datat[wh:wh2+1,i]=dar
-#             else:
-#                 datat[wh:wh2+1,i]=missing
-#     datat=datat.reshape((shapei))
-    
-#     return datat.transpose(ind), tf
-                
+    return datat, tf+time[0]
